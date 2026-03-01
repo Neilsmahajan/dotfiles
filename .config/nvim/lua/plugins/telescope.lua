a6f1aab141975f1362519313ce07cb3926a1d022
Mahajan, Neil <neil.mahajan@netapp.com>
2026-03-01 11:28:34 -0500

    Update nvim config2

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@
 
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  branch = "master",
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>",                            desc = "Find files" },
